fb9c392d5bb70878c0e1c0c8ec002ea1edb12512
Farzan <farzan.dehbashi@achivers.com>
2024-07-17 17:51:28 -0400

    .

--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/tilt-dev/tilt-example-go/handler"
+	"github.com/tilt-dev/tilt-example-go/internal/handler"
 )
 
 func main() {
 	http.Handle("/", handler.NewExampleRouter())
-
-	log.Println("Serving on port 8000")
-
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatalf("Server exited with: %v", err)
